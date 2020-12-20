3bd94a426494d31caeacea1026ae3364fd05f76f
Lester <lester.castilloc89@gmail.com>
2020-12-19 19:35:52 -0500

    Setting some data to the store, #7

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,14 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     agentListStore: [
-      { name: 'Agente 1', background: '#03DCED', id: 1, repository: '123', target: '234', command: '4324', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'asdasdad' },
-      { name: 'Agente 2', background: '#6610f2', id: 2, repository: '123', target: '234', command: '4324', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'asdasdad' },
-      { name: 'Agente 3', background: '#03DCED', id: 3, repository: '123', target: '234', command: '4324', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'asdasdad' },
-      { name: 'Agente 4', background: '#ff5400', id: 4, repository: '123', target: '234', command: '4324', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'asdasdad' },
-      { name: 'Agente 5', background: '#FF4343', id: 5, repository: '123', target: '234', command: '4324', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'asdasdad' },
-      { name: 'Agente 6', background: '#0ee06a', id: 6, repository: '123', target: '234', command: '4324', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'asdasdad' },
-      { name: 'Agente 7', background: '#03DCED', id: 7, repository: '123', target: '234', command: '4324', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'asdasdad' },
-      { name: 'Agente 8', background: '#6610f2', id: 8, repository: '123', target: '234', command: '4324', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'asdasdad' }
+      { name: 'Agente 1', background: '#03DCED', id: 1, repository: 'repository1.com', target: 'target 1', command: 'command 1', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'run agent 1', image: '' },
+      { name: 'Agente 2', background: '#6610f2', id: 2, repository: 'repository2.com', target: 'target 2', command: 'command 2', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'run agent  2', image: '' },
+      { name: 'Agente 3', background: '#03DCED', id: 3, repository: 'repository3.com', target: 'target 3', command: 'command 3', isTargetType: true, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'run agent 3', image: '' },
+      { name: 'Agente 4', background: '#ff5400', id: 4, repository: 'repository4.com', target: 'target 4', command: 'command 4', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'run agent 4', image: '' },
+      { name: 'Agente 5', background: '#FF4343', id: 5, repository: 'repository5.com', target: 'target 5', command: 'command 5', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'run agent 5', image: '' },
+      { name: 'Agente 6', background: '#0ee06a', id: 6, repository: 'repository6.com', target: 'target 6', command: 'command 6', isTargetType: true, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'run agent 6', image: '' },
+      { name: 'Agente 7', background: '#03DCED', id: 7, repository: 'repository7.com', target: 'target 7', command: 'command 7', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'run agent 7', image: '' },
+      { name: 'Agente 8', background: '#6610f2', id: 8, repository: 'repository8.com', target: 'target 8', command: 'command 8', isTargetType: false, isRootDomainType: true, isSubDomainType: false, isAliveTrigger: true, isHttpOpenTrigger: false, script: 'run agent 8', image: '' }
     ],
     idAgent: -1
   },
