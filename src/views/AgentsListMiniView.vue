82257bfa6ab138efa8cf93ca23f471c06d155ed6
Lester <lester.castilloc89@gmail.com>
2021-01-02 01:51:03 -0500

    Fixed a bug related to the confirmation form to delete an agent from the mini view, #7

--- src/views/AgentsListMiniView.vue
+++ src/views/AgentsListMiniView.vue
@@ -9,6 +9,7 @@
           <div class="row">
             <AgentMiniView v-for="agent of arrayFilterList" :key="agent.id" :id="agent.id" :name="agent.name" :background="agent.background"></AgentMiniView>
           </div>
+          <AgentConfirmation></AgentConfirmation>
         </div>
       </div>
     </div>
@@ -20,13 +21,15 @@ import AgentMiniView from '@/components/AgentMiniView.vue'
 import NavBarTwo from '@/components/NavBarTwo.vue'
 import { mapState, mapGetters } from 'vuex'
 import AgentForm from '@/components/AgentForm.vue'
+import AgentConfirmation from '@/components/AgentConfirmation.vue'
 
 export default {
   name: 'AgentListMiniView',
   components: {
     AgentMiniView,
     NavBarTwo,
-    AgentForm
+    AgentForm,
+    AgentConfirmation
   },
   computed: {
     ...mapState(['agentListStore']),
