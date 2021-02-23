755e3f2eb1abb418b75e02f8b41871a695f7b386
Lester <lester.castilloc89@gmail.com>
2021-02-23 01:21:44 -0500

    Now when removing a target from its details page a notification is shown if the removal was successful

--- src/views/TargetsListView.vue
+++ src/views/TargetsListView.vue
@@ -12,6 +12,7 @@
           <TargetMiniList v-for="target of arrayFilterList" :key="target.id" :id="target.id" :name="target.name" :primaryColor="target.primaryColor" :secondaryColor= "target.secondaryColor" :rootDom="target.rootDomains">
           </TargetMiniList>
           </div>
+          <Toast :baseZIndex="200"/>
         </div>
       </div>
     </div>
@@ -20,17 +21,19 @@
 <script>
 import TargetsList from '@/components/Target/TargetsList.vue'
 import NavBarTwoTarget from '@/components/Target/NavBarTwoTarget.vue'
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters, mapMutations } from 'vuex'
 import TargetMiniList from '@/components/Target/TargetMiniList.vue'
+import Toast from 'primevue/toast'
 export default {
   name: 'TargetsListView',
   components: {
     TargetsList,
     NavBarTwoTarget,
-    TargetMiniList
+    TargetMiniList,
+    Toast
   },
   computed: {
-    ...mapState('target', ['targetListStore', 'filterColour']),
+    ...mapState('target', ['targetListStore', 'filterColour', 'isTargetDeleted']),
     ...mapGetters('target', ['filterByColor']),
     arrayFilterList () {
       if (this.filterColour === '') {
@@ -40,8 +43,15 @@ export default {
       }
     }
   },
+  methods: {
+    ...mapMutations('target', ['setIsTargetDeleted'])
+  },
   mounted () {
     this.$store.commit('updateLocView', 'Targets', true)
+    if (this.isTargetDeleted) {
+      this.$toast.add({ severity: 'success', sumary: 'Success', detail: 'The target has been deleted successfully', life: 3000 })
+      this.setIsTargetDeleted(false)
+    }
   }
 }
 </script>
