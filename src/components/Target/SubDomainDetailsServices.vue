3a1e0ed6086c3a13b4d128890230c8fd5be7bad2
Lester <lester.castilloc89@gmail.com>
2021-06-18 23:54:38 -0400

    Added to agent, service and directory listings in subdomain and rootdomain detail views a message for when no items exist

--- src/components/Target/SubDomainDetailsServices.vue
+++ src/components/Target/SubDomainDetailsServices.vue
@@ -3,7 +3,7 @@
     <h6 class="text-center mt-2">List of Services</h6>
   <div class="card card-style">
     <div class="card-body">
-      <div class="card card-table">
+      <div v-if="listServices.length > 0" class="card card-table">
         <div class=" row mb-2"  >
           <div class="col border-left-radius border-right text-light-white domain-names-list p-2" v-bind:style ="{'background':color}"> <p class="ml-2 m-0" v-on:click="orderByName()"> Name
            <i class="material-icons right float-right" v-show="active_arrow_down">keyboard_arrow_down</i>
@@ -14,10 +14,15 @@
            <i class="material-icons right float-right" v-show="active_arrow_port_up">keyboard_arrow_up</i>
           </div>
         </div>
-      <div class="row mb-2" v-for="item of listServices" :key="item.id">
-        <div class="col  border-left-radius border"><p class="m-2"> {{item.name}}</p></div>
-        <div class="col border border-left-0 border-right-radius text-center"><p class="m-2">{{item.port}}</p></div>
+        <div class="row mb-2" v-for="item of listServices" :key="item.id">
+          <div class="col  border-left-radius border"><p class="m-2"> {{item.name}}</p></div>
+          <div class="col border border-left-0 border-right-radius text-center"><p class="m-2">{{item.port}}</p></div>
+        </div>
       </div>
+      <div v-else>
+        <p class="lead text-center">
+          There are no services associated with this subdomain.
+        </p>
       </div>
     </div>
   </div></div>
