278c05e75d0ed4933747497064547fa74d5718d1
Lester <lester.castilloc89@gmail.com>
2022-02-27 16:55:02 -0800

    Modify the interface to manage the wordlists
    * Increase the size of the interface
    * Increase the size of the Path column by reducing the size of the Actions and Count columns
    Resolves: #484

--- src/components/General/Wordlist.vue
+++ src/components/General/Wordlist.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="col-12">
   <div class="modal fade" id="wordlistModal" tabindex="-1" role="dialog" aria-labelledby="wordlistModal" aria-hidden="true" data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
       <div class="modal-content agent-containers">
         <div class="modal-header remove-flex">
           <div class="row">
@@ -38,18 +38,18 @@
               <div class="wordlist-files-container wordlist-container-size border px-2 py-1 mt-2">
                 <div class="row">
                   <div class="col-2 border-bottom pb-1"><span class="font-weight-bold">Filename</span></div>
-                  <div class="col-2 border-left border-right border-bottom pb-1"><span class="font-weight-bold">Count</span></div>
+                  <div class="col-1 border-left border-right border-bottom pb-1"><span class="font-weight-bold">Count</span></div>
                   <div class="col-1 border-right border-bottom pb-1"><span class="font-weight-bold">Size</span></div>
-                  <div class="col-4 border-right border-bottom pb-1"><span class="font-weight-bold">Path</span></div>
-                  <div class="col-3 border-bottom pb-1"><span class="font-weight-bold">Actions</span></div>
+                  <div class="col-6 border-right border-bottom pb-1"><span class="font-weight-bold">Path</span></div>
+                  <div class="col-2 border-bottom pb-1"><span class="font-weight-bold">Actions</span></div>
                 </div>
                 <div class=" wordlist-data-container overflow-y-auto w-100">
                 <div class="row pt-3" v-for="wordlistItem of getWordListByType" :key="wordlistItem.id">
                   <div class="col-2"><span class="text-wrap break-word">{{wordlistItem.filename}}</span></div>
-                  <div class="col-2"><span class="text-wrap break-word">{{wordlistItem.count}}</span></div>
+                  <div class="col-1"><span class="text-wrap break-word">{{wordlistItem.count}}</span></div>
                   <div class="col-1"><span class="text-wrap break-word">{{this.$niceBytes(wordlistItem.size)}}</span></div>
-                  <div class="col-4"><span class="text-break break-word">{{wordlistItem.path}}/{{wordlistItem.filename}}</span></div>
-                  <div class="col-3">
+                  <div class="col-6"><span class="text-break break-word">{{wordlistItem.path}}/{{wordlistItem.filename}}</span></div>
+                  <div class="col-2">
                     <div class="d-flex justify-content-between">
                       <button @click="downloadWordlist" type="button" :data-id="wordlistItem.id" :data-name="wordlistItem.filename" class="wordlist-btn-size blue-text agent-border btn create-agent-buttons-main-action rounded wordlist-download-btn">Download</button>
                       <button @click="removeWordList" type="button" :data-id="wordlistItem.id" :data-name="wordlistItem.filename" class="wordlist-btn-size red-text agent-border btn create-agent-buttons-main-action rounded">Delete</button>
