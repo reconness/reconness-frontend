fe7dff376f7b218efedf5ea8890b4cb4e9cd9657
Lester <lester.castilloc89@gmail.com>
2022-06-02 18:46:59 +0000

    Create a component to indicate a running process

---
+++ src/components/General/SpinnerCircle.vue
@@ -0,0 +1,48 @@
+<template>
+    <div v-show="show" :class="cssClass"></div>
+</template>
+<script>
+export default {
+  name: 'SpinnerCircle',
+  props: {
+    addCss: {
+      type: String,
+      default: ''
+    },
+    show: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    cssClass () {
+      const classes = ['sbl-circ']
+      if ((this.addCss || '').trim().length > 0) {
+        classes.push(this.addCss.trim())
+      }
+      return classes.join(' ')
+    }
+  }
+}
+</script>
+<style>
+.sbl-circ {
+  height: 48px;
+  width: 48px;
+  color: #5a5a5a;
+  position: relative;
+  display: inline;
+  border: 2px solid;
+  border-radius: 50%;
+  border-top-color: transparent;
+  animation: rotate 1s linear infinite;
+}
+@keyframes rotate {
+    0% {
+        transform: rotate(0);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
