d6599ced07c8ce6a1f0fe7ab80f2f4fce1b83c29
Lester <lester.castilloc89@gmail.com>
2022-06-04 19:30:15 +0000

    Refactor system components where the clickOutside directive is used locally
    Remove the code where the directive is defined

--- src/components/General/AutoCompleteLocations.vue
+++ src/components/General/AutoCompleteLocations.vue
@@ -48,21 +48,6 @@ export default {
     closeMenuFn () {
       this.closedMenu = true
     }
-  },
-  directives: {
-    clickOutside: {
-      beforeMount (el, binding, vnode) {
-        el.clickOutsideEvent = function (event) {
-          if (!(el === event.target || el.contains(event.target))) {
-            binding.value(event, el)
-          }
-        }
-        document.body.addEventListener('click', el.clickOutsideEvent)
-      },
-      unmounted (el) {
-        document.body.removeEventListener('click', el.clickOutsideEvent)
-      }
-    }
   }
 }
 </script>
--- src/components/General/NotesSection.vue
+++ src/components/General/NotesSection.vue
@@ -324,21 +324,6 @@ export default {
         this.sortDescendingOrderByUserNameFn
       )
     }
-  },
-  directives: {
-    clickOutside: {
-      beforeMount (el, binding, vnode) {
-        el.clickOutsideEvent = function (event) {
-          if (!(el === event.target || el.contains(event.target))) {
-            binding.value(event, el)
-          }
-        }
-        document.body.addEventListener('click', el.clickOutsideEvent)
-      },
-      unmounted (el) {
-        document.body.removeEventListener('click', el.clickOutsideEvent)
-      }
-    }
   }
 }
 </script>
