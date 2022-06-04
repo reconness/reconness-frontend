6a9de236590019dfdcea94af86275e52f36f251f
Lester <lester.castilloc89@gmail.com>
2022-06-04 19:12:32 +0000

    Refactor the code related to the clickOutside directive
    Set the clickOutside directive globally to the system

--- src/main.js
+++ src/main.js
@@ -18,4 +18,20 @@ import '@hennge/vue3-pagination/dist/vue3-pagination.css'
 axios.defaults.baseURL = `https://${process.env.VUE_APP_API_RECONNES_HOST}/api`
 Popper.Defaults.modifiers.computeStyle.gpuAcceleration = false
 
-createApp(App).use(store).use(router).use(PrimeVue).use(VueApexCharts).use(Reconness).mount('#app')
+const app = createApp(App)
+
+app.directive('clickOutside', {
+  beforeMount (el, binding, vnode) {
+    el.clickOutsideEvent = function (event) {
+      if (!(el === event.target || el.contains(event.target))) {
+        binding.value(event, el)
+      }
+    }
+    document.body.addEventListener('click', el.clickOutsideEvent)
+  },
+  unmounted (el) {
+    document.body.removeEventListener('click', el.clickOutsideEvent)
+  }
+})
+
+app.use(store).use(router).use(PrimeVue).use(VueApexCharts).use(Reconness).mount('#app')
