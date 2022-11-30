c80476ccd7a1064ed7304578a01a0f73ea60a7da
Lester <lester.castilloc89@gmail.com>
2022-11-30 00:09:18 +0000

    Refactors the system home page
    * Create  a list with the information of the subdomains by directory
    * Inserts the data to be displayed in the TargetsHighestInteraction component

--- src/views/SystemHome.vue
+++ src/views/SystemHome.vue
@@ -31,13 +31,13 @@
                     <p class="float-right bold-text separator-content inline-material-icons">Last week resumee<span class="material-icons search-icon blue-text">keyboard_arrow_right</span></p>
                   </div>
                   <div class="col-12 col-lg-4 pr-4">
-                  <TargetsHighestInteraction :background="'#03dced'" :title= "'Targets with the most interactions in the last week'"></TargetsHighestInteraction>
+                  <TargetsHighestInteraction :background="'#03dced'" :title= "'Targets with the most interactions in the last week'" :subdomainByDirectories="subdomainByDirectories"></TargetsHighestInteraction>
                   </div>
                   <div class="col-12 col-lg-4">
                   <DaysHighestInteraction></DaysHighestInteraction>
                   </div>
                   <div class="col-12 col-lg-4 pl-4">
-                  <TargetsHighestInteraction :background="'rgb(134, 98, 202)'" :title= "'Targets with the most interactions in the last week'"></TargetsHighestInteraction>
+                  <TargetsHighestInteraction :background="'rgb(134, 98, 202)'" :title= "'Targets with the most interactions in the last week'" :subdomainByDirectories="subdomainByDirectories"></TargetsHighestInteraction>
                   </div>
                   </div></div>
                   <div class="col-lg-12">
@@ -235,7 +235,8 @@ export default {
         blank: {
           name: true
         }
-      }
+      },
+      subdomainByDirectories: [{ port: 88, subdomain: 'Portraite' }, { port: 70, subdomain: 'Yanet' }, { port: 60, subdomain: 'Portraite' }, { port: 51, subdomain: 'Yanet' }]
     }
   }
 }
